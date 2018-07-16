95d03f09915a53d8f8e536f175d57feae3d2d284
TobiMichael <tobiasmichael@mail.de>
2018-07-16 13:28:35 +0200

    Using pywal now; updates...

---
+++ screenlayout/checkscreens.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+connected=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | sed -n 2p)
+
+if [ ! -z "$connected" ]; then
+  sh ~/.screenlayout/withTV.sh
+  sleep 1
+  feh --bg-fill --randomize ~/.config/background/*
+  ~/.config/scripts/pywal.sh
+  notify-send "Screen" "Second screen added..."
+else
+  sh ~/.screenlayout/withoutTV.sh
+  sleep 1
+  feh --bg-fill --randomize ~/.config/background/*
+  ~/.config/scripts/pywal.sh
+  notify-send "Screen" "Second screen removed..."
+fi
