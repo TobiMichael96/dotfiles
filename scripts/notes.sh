ba54968d87ff7dae2df0d64b82b60f9fa84edcfc
TobiMichael <tobiasmichael@mail.de>
2018-09-06 13:10:09 +0200

    Reworked notes

--- scripts/notes.sh
+++ scripts/notes.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-file="/home/tobias/Nextcloud/Notes/Ablage.txt"
+file="/home/tobias/Nextcloud/Notes/Ablage/Note_"
+file+="$(date +'%d-%m-%Y')"
+file+=".txt"
 
+echo -n $(date +'%R') >> $file
+echo -n ": " >> $file
 echo $@ >> $file
 echo -ne "\n" >> $file
 echo -n "---" >> $file
