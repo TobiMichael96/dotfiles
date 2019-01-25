eef4e9e77be3b1cb7411aa6ad7834d4fc46a399a
Tobias Effner <tobias.effner@bowbridge.net>
2019-01-25 12:03:25 +0100

    Updated mount script to use other rights

--- scripts/mount-stick.sh
+++ scripts/mount-stick.sh
@@ -1,2 +1,2 @@
 sudo cryptsetup luksOpen /dev/disk/by-uuid/504d8a98-ab79-4aa8-9d6c-d7205c4a6cfe Stick
-sudo mount /dev/mapper/Stick ~/Stick
+sudo mount -o uid=tobias,umask=077 /dev/mapper/Stick ~/Stick
