881d0610412f9dbbc66f4286a595fba8f246dd4b
Tobias <tobiasmichael@mail.de>
2019-02-17 21:56:53 +0100

    Added power menu script, removed led script

--- scripts/led.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ $1 -eq 1 ]; then
-	wget http://192.168.2.53/cm?cmnd=Power%20on 2>/dev/null
-else
-	wget http://192.168.2.53/cm?cmnd=Power%20off 2>/dev/null
-fi
---
+++ scripts/power_menu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#MENU="$(rofi -sep '|' -dmenu -i -p 'System' -hide-scrollbar echo -e " Lock| Logout| Reboot")"
+MENU="$(echo " Lock| Logout| Reboot| Shutdown" | rofi -sep '|' -dmenu -i -p "System: ")"
+
+
+case "$MENU" in
+	*Lock) sh ~/.config/scripts/physlock.sh ;;
+        *Logout) bspc quit ;;
+        *Reboot) systemctl reboot ;;
+        *Shutdown) systemctl -i poweroff ;;
+esac
