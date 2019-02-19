b856897b4d9eed8868126121dbef19a1f7186ad6
Tobias <tobiasmichael@mail.de>
2019-02-19 19:48:51 +0100

    Changed order in autostart; updated mount stick script

--- scripts/mount-stick.sh
+++ scripts/mount-stick.sh
@@ -1,2 +1,6 @@
 sudo cryptsetup luksOpen /dev/disk/by-uuid/504d8a98-ab79-4aa8-9d6c-d7205c4a6cfe Stick
 sudo mount -o uid=tobias,umask=277 /dev/mapper/Stick ~/Stick
+
+if (df -aTh | grep "/home/tobias/Nextcloud" > /dev/null); then
+	sudo cp /home/tobias/Nextcloud/Passwords/Passwords.kdbx ~/Stick/.backup/Passwords_backup.kdbx
+fi
