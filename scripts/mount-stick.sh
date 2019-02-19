4a19b43947bd1b7590e7c9f7f9eb8232b11c80b4
Tobias <tobiasmichael@mail.de>
2019-02-19 20:15:23 +0100

    Fixed mount script

--- scripts/mount-stick.sh
+++ scripts/mount-stick.sh
@@ -1,4 +1,11 @@
-sudo cryptsetup luksOpen /dev/disk/by-uuid/504d8a98-ab79-4aa8-9d6c-d7205c4a6cfe Stick
+stick=31df4720-2a20-4a51-b898-eeaad29522c6
+
+if (ls /dev/disk/by-uuid | grep "504d8a98-ab79-4aa8-9d6c-d7205c4a6cfe" > /dev/null); then
+	stick=504d8a98-ab79-4aa8-9d6c-d7205c4a6cfe
+fi
+stick="/dev/disk/by-uuid/$stick"
+
+sudo cryptsetup luksOpen $stick Stick
 sudo mount -o uid=tobias,umask=277 /dev/mapper/Stick ~/Stick
 
 if (df -aTh | grep "/home/tobias/Nextcloud" > /dev/null); then
