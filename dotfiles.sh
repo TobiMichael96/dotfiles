0bf4e443adc59d206f1d7c0c0368cb6f3925bd09
Tobias Effner <tobias.effner@bowbridge.net>
2019-02-22 12:28:29 +0100

    Automatic update at Fri 22 Feb 2019 12:28:29 PM CET.

--- dotfiles.sh
+++ dotfiles.sh
@@ -74,7 +74,7 @@ backup_files() {
         CHANGED=$(git diff-index --name-only HEAD --)
 	if [ -n "$CHANGED" ]; then
                 git commit -m "Automatic update in submodule at $(date +"%c")."
-                git push >/dev/null
+                git push 
 	else 
 		echo "No changes in submodule to push to repository..."
         fi
@@ -90,7 +90,7 @@ backup_files() {
 	if [ -n "$CHANGED" ]; then
 		echo "Pushing changes now..."
 		git commit -m "Automatic update at $(date +"%c")."
-		git push >/dev/null
+		git push 
 	else
 		echo "No changes to push to repository..."
 	fi
