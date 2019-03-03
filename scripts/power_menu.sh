991c617bf3ac6deac2800c428d731c4a291f5771
TobiMichael <tobiasmichael@mail.de>
2019-03-03 10:49:51 +0100

    Automatic update at Sun 03 Mar 2019 10:49:51 AM CET.

--- scripts/power_menu.sh
+++ scripts/power_menu.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+user=$(who)
+
 #MENU="$(rofi -sep '|' -dmenu -i -p 'System' -hide-scrollbar echo -e " Lock| Logout| Reboot")"
 MENU="$(echo " Lock| Logout| Reboot| Shutdown" | rofi -sep '|' -dmenu -i -p "System: ")"
 
 
 case "$MENU" in
 	*Lock) sh ~/.config/scripts/physlock.sh ;;
-        *Logout) bspc quit && exit ;;
+        *Logout) bspc quit && umount /home/tobias/Nextcloud && pkill -KILL -u $user ;;
         *Reboot) systemctl reboot ;;
         *Shutdown) systemctl -i poweroff ;;
 esac
