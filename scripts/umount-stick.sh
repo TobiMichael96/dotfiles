e3fa6452f62c8d24a00524b140fa02c5a1ce7a15
TobiMichael <tobiasmichael@mail.de>
2019-05-14 10:17:44 +0200

    Automatic update at Tue 14 May 2019 10:17:44 AM CEST.

--- scripts/umount-stick.sh
+++
@@ -1,2 +0,0 @@
-sudo umount ~/Stick
-sudo cryptsetup luksClose /dev/mapper/Stick
--- scripts/wireguard.sh
+++ scripts/wireguard.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo wg-quick $1 ~/Stick/homeserver-wg0.conf
+sudo wg-quick $1 /etc/wireguard/laptop-wg0.conf
