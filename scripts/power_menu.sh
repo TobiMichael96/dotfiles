e8e3f2e1d0f527ac7e84c055383c2e195b0791d4
TobiMichael <tobiasmichael@mail.de>
2019-06-21 13:48:00 +0200

    Automatic update at Fri 21 Jun 2019 01:48:00 PM CEST.

--- scripts/power_menu.sh
+++ scripts/power_menu.sh
@@ -3,12 +3,13 @@
 user=$(who)
 
 #MENU="$(rofi -sep '|' -dmenu -i -p 'System' -hide-scrollbar echo -e " Lock| Logout| Reboot")"
-MENU="$(echo " Lock| Logout| Reboot| Shutdown" | rofi -sep '|' -dmenu -i -p "System: ")"
+MENU="$(echo "	Lock|	Logout|	Suspend|	Reboot|	Shutdown" | rofi -sep '|' -dmenu -i -p "System: ")"
 
 
 case "$MENU" in
 	*Lock) sh ~/dotfiles/scripts/lock.sh ;;
         *Logout) bspc quit && umount /home/tobias/Nextcloud && pkill -KILL -u $user ;;
+	*Suspend) systemctl suspend ;;
         *Reboot) systemctl reboot ;;
         *Shutdown) systemctl -i poweroff ;;
 esac
