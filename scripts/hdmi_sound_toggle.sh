eeddb7264084da8ee9023a64682545cd8285c8dc
Tobias <tobiasmichael@mail.de>
2019-08-21 13:40:28 +0200

    Automatic update at Wed 21 Aug 2019 01:40:28 PM CEST.

--- scripts/hdmi_sound_toggle.sh
+++ scripts/hdmi_sound_toggle.sh
@@ -6,20 +6,26 @@ CARD_PATH="/sys/class/drm/card0/"
 AUDIO_OUTPUT="analog-surround-40"
 PULSE_SERVER="unix:/run/user/"$USER_ID"/pulse/native"
 
-for OUTPUT in $(cd "$CARD_PATH" && echo card*); do
-  OUT_STATUS=$(<"$CARD_PATH"/"$OUTPUT"/status)
-  if [[ $OUT_STATUS == connected ]]
-  then
-    echo $OUTPUT connected
-    case "$OUTPUT" in
-      "card0-HDMI-A-1")
-        AUDIO_OUTPUT="hdmi-stereo" # Digital Stereo (HDMI1)
-     ;;
-      "card0-HDMI-A-2")
-        AUDIO_OUTPUT="hdmi-stereo-extra1" # Digital Stereo (HDMI 2)
-     ;;
-    esac
-  fi
-done
-echo selecting output $AUDIO_OUTPUT
-sudo -u "$USER_NAME" pactl --server "$PULSE_SERVER" set-card-profile 0 output:$AUDIO_OUTPUT+input:analog-stereo
+#for OUTPUT in $(cd "$CARD_PATH" && echo card*); do
+#  OUT_STATUS=$(<"$CARD_PATH"/"$OUTPUT"/status)
+#  if [[ $OUT_STATUS == connected ]]
+#  then
+#    echo $OUTPUT connected
+#    case "$OUTPUT" in
+#      "card0-HDMI-A-1")
+#        AUDIO_OUTPUT="hdmi-stereo" # Digital Stereo (HDMI1)
+#     ;;
+#      "card0-HDMI-A-2")
+#        AUDIO_OUTPUT="hdmi-stereo-extra1" # Digital Stereo (HDMI 2)
+#     ;;
+#    esac
+#  fi
+#done
+#echo selecting output $AUDIO_OUTPUT
+#sudo -u "$USER_NAME" pactl --server "$PULSE_SERVER" set-card-profile 0 output:$AUDIO_OUTPUT+input:analog-stereo
+
+if [[ ! -z $(xrandr -q | grep "HDMI1 connected" | cut -d ' ' -f3 | sed 's/[^0-9]*//g') ]]; then
+	pacmd set-default-sink alsa_output.pci-0000_00_1b.0.hdmi-stereo
+else
+	pacmd set-default-sink Analog
+fi
--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-number=$(pactl list short sinks | grep "1b.0.analog-stereo" | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,')
-volume=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $number + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' )
+number=$(pactl list short sinks | grep "Analog" | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,')
+volume=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $number | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' )
+mute=$(pactl list sinks | grep '^[[:space:]]Mute:' | head -n $number | tail -n 1 | sed -e 's/^[ \t]*//')
 output="Volume: $volume%"
 
 notify() {
-	notify-send -u low "Speaker Volume" "$output"
+	notify-send -u low "$output" "$mute"
 }
 
 output() {
