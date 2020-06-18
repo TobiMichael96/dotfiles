294bd6084486adf571cc299544ded19b3a829afd
Tobias Effner <me@tobiasmichael.de>
2020-06-18 13:08:18 +0200

    Automatic update at Thu 18 Jun 2020 01:08:18 PM CEST.

--- scripts/move_around.sh
+++ scripts/move_around.sh
@@ -1,22 +1,27 @@
 #!/bin/env sh
 DIRECTION=$1
 
-FOCUSED_NODE=$(bspc query -N -n focused.local)
-
+# prepare parameters
 if [ "$1" = "next" ]; then
-	FOCUSED="biggest"
-	TO_FOCUS="smallest"
+	FOCUSED="east"
+	TO_FOCUS="west"
 else
-	FOCUSED="smallest"
-	TO_FOCUS="biggest"
+	FOCUSED="west"
+	TO_FOCUS="east"
 fi
 
+# get most left or most right node
+IMPORTANT_NODE=$(bspc query -N -n $FOCUSED)
+# check if there is a node under the most right node
+SECOND_NODE=$(bspc query -N -n south)
 
-IMPORTANT_NODE=$(bspc query -N -n $FOCUSED.tiled.local)
-
-if [ "$FOCUSED_NODE" = "$IMPORTANT_NODE" ]; then
-	bspc desktop -f $1.local
-	bspc node --focus $TO_FOCUS.local
+if [ -z "$IMPORTANT_NODE" ]; then
+	if [ -z "$SECOND_NODE" ]; then
+		bspc desktop -f $1.local
+		bspc node --focus $TO_FOCUS.local
+	else
+		bspc node --focus $1.local
+	fi
 else
 	bspc node --focus $1.local
 fi
